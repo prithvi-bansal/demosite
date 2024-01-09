66f44459b3bb0a06a11584418326daa62783afc9
prithvi-bansal <prithvi.bansal1000@gmail.com>
2024-01-09 11:39:20 +0530

    test cases and pipeline test

--- demosite/apps/products/tests.py
+++ demosite/apps/products/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+
+def demo_test_dashboard(self):
+    client = Client()
+    url = reverse('dashboard')
+    response = client.get(url)
+    self.assertEqual(response.status_code, 200)
+
+
+def demo_test_summary(self):
+    client = Client()
+    url = reverse('summary')
+    response = client.get(url)
+    self.assertEqual(response.status_code, 200)
