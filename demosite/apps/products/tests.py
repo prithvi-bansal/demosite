496c5fea6ba84d888254a7d8c2bd38b7c9134a04
prithvi-bansal <prithvi.bansal1000@gmail.com>
2024-01-09 12:26:02 +0530

    changes in test cases

--- demosite/apps/products/tests.py
+++ demosite/apps/products/tests.py
@@ -1,17 +1,27 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from .models import User
 
 # Create your tests here.
 
-def demo_test_dashboard(self):
-    client = Client()
-    url = reverse('dashboard')
-    response = client.get(url)
-    self.assertEqual(response.status_code, 200)
+class DemoTestCase(TestCase):
 
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
 
-def demo_test_summary(self):
-    client = Client()
-    url = reverse('summary')
-    response = client.get(url)
-    self.assertEqual(response.status_code, 200)
+    def test_demo_dashboard(self):
+        client = Client()
+        url = reverse('dashboard')
+        client.force_login(self.user)
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_demo_summary(self):
+        client = Client()
+        client.force_login(self.user)
+        url = reverse('summary')
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def tearDown(self):
+        self.user.delete()
